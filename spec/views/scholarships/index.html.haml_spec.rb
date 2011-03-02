05754e35df31cb9dd4eb9bd01d8352e52c3dfb66
christian LAPOINTE <clapointe@pyxis-tech.com>
2011-03-02 15:18:39 -0500

    adding view test and spec_helper

--- spec/views/scholarships/index.html.haml_spec.rb
+++ spec/views/scholarships/index.html.haml_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe "scholarships/index.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should have a search action" do
+
+  assign :scholarships, []
+  render
+
+  view_content_for(:colonneGauche).should have_selector('form', :action => '/scholarships/search')
+  end
+
+
 end
